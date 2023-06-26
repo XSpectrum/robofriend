37cf382e66783f64fc08e23020d22b2a8381db6c
XSpectrum <fadel.dafia@epitech.eu>
2023-06-26 20:46:17 +0100

    [ADD] get data from server

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 import React, { Component } from "react";
 import SearchBox from "./SearchBox";
 import CardList from "./components/CardList";
-import { robots } from "./constants/robots";
 
 class App extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            robots: robots,
-            searchfield : ''
+            robots: [],
+            searchfield: ''
         }
     }
 
     onSearchChange = (event) => {
         this.setState({
             searchfield: event.target.value,
-        });       
-    } 
+        });
+    }
 
-    render(){
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users => {
+            this.setState({
+                robots: users
+            });
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+    render() {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-        }) 
+        })
 
-        return (
-            <div className='flex flex-col items-center'>
-                <h1 className="font-bold text-lg my-5 ">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <div className='my-5 h-1 w-full bg-gray-700'> </div>
-                <CardList robots={filteredRobots} />
-            </div>
-        )
+        if(this.state.robots.length === 0 ){
+            return (
+                <div className='flex flex-col items-center'>
+                    <h1>loading...</h1>
+                </div>
+            )
+        }else{
+            return (
+                <div className='flex flex-col items-center'>
+                    <h1 className="font-bold text-lg my-5 ">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <div className='my-5 h-1 w-full bg-gray-700'> </div>
+                    <CardList robots={filteredRobots} />
+                </div>
+            )
+        }
     }
 };
 
